5db8cd7e6a7f2bad89fdd7f32184390a989b476c
kimdal <kimdal1029@naver.com>
2021-11-28 18:45:54 +0900

    NEW

--- sketch.js
+++ sketch.js
@@ -1,72 +1,7 @@
-// Benedikt Groß
-// Example is based on examples from: http://brm.io/matter-js/, https://github.com/shiffman/p5-matter
-
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Mouse = Matter.Mouse;
-const MouseConstraint = Matter.MouseConstraint;
-const Composites = Matter.Composites;
-
-const drawMouse = Helpers.drawMouse;
-const drawBody = Helpers.drawBody;
-const drawBodies = Helpers.drawBodies;
-
-let engine;
-let ground;
-//let balls;
-let boxes;
-let GrounfWater;
-
 function setup() {
-  const canvas = createCanvas(635, 800);
-  let GroundWater = new GW();
-
-  // create an engine
-  engine = Engine.create();
-
-  // add boxes
-  // xx, yy, columns, rows, columnGap, rowGap
-  boxes = Composites.stack(1, 0, 28, 20, 3, 3, function(x, y) {
-    return Bodies.rectangle(x, y, 20, 20);
-  });
-  // add balls
-  //balls = Composites.stack(500, 0, 2, 5, 3, 3, function(x, y) {
-  //return Bodies.circle(x, y, 50);
-
-  World.add(engine.world, [boxes]);
-
-  // ground
-  ground = Bodies.rectangle(400, height-10, 810, 30, {isStatic: true});
-  World.add(engine.world, [ground]);
-
-  // setup mouse
-  const mouse = Mouse.create(canvas.elt);
-  const mouseParams = {
-    mouse: mouse,
-    constraint: { stiffness: 0.05 }
-  }
-  mouseConstraint = MouseConstraint.create(engine, mouseParams);
-  mouseConstraint.mouse.pixelRatio = pixelDensity();
-  World.add(engine.world, mouseConstraint);
-
-  // run the engine
-  Engine.run(engine);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(0);
-
-  stroke(128);
-  strokeWeight(1);
-  fill(255);
-  drawBodies(boxes.bodies);
-  //drawBodies(balls.bodies);
-
-  noStroke();
-  fill(128);
-  drawBody(ground);
-
-  drawMouse(mouseConstraint);
+  background(220);
 }
